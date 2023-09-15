f1678c76ab2495a5364e0ec65dc1db0c721bc26b
mirzasafwan <mohammedsafwanmirza786@gmail.com>
2023-09-15 18:38:47 +0530

    second commit

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const ejs = require("ejs");
+// const ejs = require("ejs");
 const app = express();
-// app.get("/", (req, res) => {
-//   res.json({
-//     msg: "Success",
-//   });
-// });
-app.set("view engine", "ejs");
-
 app.get("/", (req, res) => {
-  res.render("index");
+  res.json({
+    msg: "Success",
+  });
 });
+// app.set("view engine", "ejs");
+
+// app.get("/", (req, res) => {
+//   res.render("index");
+// });
 app.listen(3000);
